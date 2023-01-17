05784cca83c55827bff5392c49d7bbbf8436ae62
Dethylyn <dethylyn.b@gmail.com>
2023-01-18 01:10:54 +0800

    Adding Login Page and Addition of Dashboard Assets

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,7 +1,7 @@
  <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
             <div class="container">
-                <a class="navbar-brand" href="#page-top"><img src="assets-homepage/assets/img/final-logo.png" alt="..." /></a>
+                <a class="navbar-brand" href="/"><img src="assets-homepage/assets/img/final-logo.png" alt="..." /></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars ms-1"></i>
@@ -13,6 +13,7 @@
                         <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
                         <li class="nav-item"><a class="nav-link" href="#location">Location</a></li>
                         <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link btn btn-info" href="/login">Login</a></li>
                       <!--  <a class="btn btn-primary btn-xl text-uppercase" href="#announcements">Tell Me More</a>-->
                     </ul>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('homepage');
-});
+Route::get('/', [PagesController::class, 'index']);
 
-Route::get('/login', function () {
-    return view('login');
-});
+Route::get('/login',[PagesController::class, 'login']);
+ 
